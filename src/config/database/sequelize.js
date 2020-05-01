22357d5d541ed25ede7dfb29f70897af110adc1d
Lucas Pelegrino <lucaspelegrino@ioasys.com.br>
2020-05-01 01:32:32 -0300

    style(sequelize): adding log to check config file

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -2,7 +2,13 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
-
+console.log({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.NODE_ENV !== 'test' ? process.env.DB_NAME : `${process.env.DB_NAME}-test`,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+});
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
