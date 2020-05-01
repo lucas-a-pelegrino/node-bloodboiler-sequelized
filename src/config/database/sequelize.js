e794d3ed4c5612ef8584d7742cd507c0a3d878dc
Lucas Pelegrino <lucaspelegrino@ioasys.com.br>
2020-05-01 01:38:48 -0300

    style(build): changing pre and post test commands

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -2,13 +2,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
-console.log({
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.NODE_ENV !== 'test' ? process.env.DB_NAME : `${process.env.DB_NAME}-test`,
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-});
+
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
